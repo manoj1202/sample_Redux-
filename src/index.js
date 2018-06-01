94e45e8feced190449de2e2cd45a084bd38e13cd
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-01 14:21:44 -0500

    Sample Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux'
 import allReducers from './reducers';
 import App from './App'
+
 
 const store = createStore(allReducers);
 
