aebb7e462a685d27bbe43eeeab292f6abe9b0f80
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-01 13:45:30 -0500

    Sample Redux

---
+++ src/reducers/reduceruser.js
@@ -0,0 +1,28 @@
+export default function(){
+    return[
+        {
+            id:1,
+            first:"Bucky",
+            last:"Roberts",
+            age:76,
+            description:"Bucky likes to teach",
+            thumbnail:""
+        },
+        {
+            id:2,
+            first:"joby",
+            last:"Wasilko",
+            age:63,
+            description:"joby loves the packers,chesse",
+            thumbnail:""
+        },
+        {
+            id:3,
+            first:"Madison",
+            last:"William",
+            age:36,
+            description:"Madi likes dogs",
+            thumbnail:""
+        }
+    ]
+}
