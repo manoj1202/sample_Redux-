b8de600ac6ba026fbe3094ef553d377d5a807440
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-01 17:54:37 -0500

    Sample Redux

--- src/action/index.js
+++ src/action/index.js
@@ -1,5 +1,5 @@
 export const selectUser = (user) => {
-    console.log("you have clicked on  User", user.first);
+    console.log("you have clicked on  User :", user.first);
     return {
         type: "USER_SELECTED",
         payload: user
