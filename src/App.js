d10ecad34aaa98db60dc1487e7ba8b37254a7676
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-01 15:35:45 -0500

    Sample Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './containers/user-list';
 
 class App extends Component {
   render() {
     return (
       <div >
         <h1>User List:</h1>
+        <UserList />
         <hr />
         <h1>User Details:</h1>
       </div>
---
+++ src/containers/user-list.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class UserList extends Component{
+    render(){
+        return(
+            <ul>
+                <li>one</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ul>
+        );
+    }
+}
+export default UserList
